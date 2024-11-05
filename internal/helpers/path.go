248c77c1b1dba8e10d52de5ef249a07c19928b5b
style77 <stylek777@gmail.com>
2024-11-06 00:00:05 +0100

    fix: Fix ExpandPath, update function to use os.UserHomeDir()

--- internal/helpers/path.go
+++ internal/helpers/path.go
@@ -1,21 +1,20 @@
+// helpers.go
 package helpers
 
 import (
-	"os/user"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
 // ExpandPath expands a path that starts with '~' to the user's home directory.
 func ExpandPath(path string) (string, error) {
-	// Check if the path starts with '~' and expand it
-	if strings.HasPrefix(path, "~") {
-		usr, err := user.Current()
+	if strings.HasPrefix(path, "~") && !strings.HasPrefix(path, "~~") {
+		usr, err := os.UserHomeDir()
 		if err != nil {
 			return "", err
 		}
-		// Replace '~' with the user's home directory
-		return filepath.Join(usr.HomeDir, path[1:]), nil
+		return filepath.Join(usr, path[1:]), nil
 	}
 	return path, nil
 }
