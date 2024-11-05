50d026fd6cf7761935715782a99558e9be33a116
style77 <stylek777@gmail.com>
2024-11-05 23:12:22 +0100

    feat: Add root command and configuration initialization

---
+++ cmd/root.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/style77/gas/internal/git"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gas",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: git.HandleGitCommand,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+
+	viper.AddConfigPath(home)
+	viper.SetConfigType("yaml")
+	viper.SetConfigName(".gas")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; create it
+			err := viper.WriteConfigAs(home + "/.gas.yaml")
+			if err != nil {
+				fmt.Println("Failed to create config file: ", err)
+			}
+		} else {
+			// Config file was found but another error was produced
+			fmt.Println("Failed to read config file: ", err)
+		}
+	}
+}
